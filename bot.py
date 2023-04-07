14912c0ffd4190f06438883d57aac16f2135859f
Your Name <you@example.com>
2023-04-07 16:25:34 +0700

    change

---
+++ bot.py
@@ -0,0 +1,70 @@
+from telegram import *
+from telegram.ext import *
+import requests
+import json
+from types import SimpleNamespace
+import math
+import random
+
+kyc = "👨‍💻 Xác minh KYC"
+uytin = "💎 DS Uy tín"
+
+domain = "https://chootc.com"
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    buttons = [[KeyboardButton(kyc), KeyboardButton(uytin)]]
+
+    reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Chào mừng bạn đến với <b>Hero Team</b>", parse_mode=constants.ParseMode.HTML)
+
+
+async def messageHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    username = update.effective_user.username
+    chat_id = update.effective_chat.id
+
+app = ApplicationBuilder().token(
+    "6274365100:AAGi5Mh9fKUNfdOzKUCIH7gauVWfn2Dvy-Y").build()
+
+app.add_handler(CommandHandler("start", start)) 
+app.add_handler(MessageHandler(filters.ALL, messageHandler))
+
+
+# auto send message
+async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
+
+    buy = requests.get(
+        f"{domain}/api/p2p?type=buy&asset=usdt&fiat=vnd")
+    sell = requests.get(
+        f"{domain}/api/p2p?type=sell&asset=usdt&fiat=vnd")
+
+    krw_res = requests.get(
+        f"{domain}/api/rate/bank")
+
+    buy_price = buy.json()['data'][4]['adv']['price']
+    sell_price = sell.json()['data'][4]['adv']['price']
+
+    krw = krw_res.json()[10]
+
+    message = f"<b>USDT</b>\nBán: <b>{int(buy_price):,} VND</b>\nMua: <b>{int(sell_price):,} VND</b>\n\n<b>KRW</b>\nBán: <b>{krw['sell']} VND</b>\nMua: <b>{krw['buy']} VND</b>\n\n<b>Liên hệ:</b>\nTelegram: @business1221\nSĐT: 094.797.8888\nXem tỷ giá miễn phí tại: https://chootc.com"
+
+    res = requests.get(
+        f"{domain}/api/setup")
+    last_msg_id = res.json()[0]["value"]
+
+    await context.bot.delete_message(message_id=last_msg_id, chat_id='-1001871429218')
+    msg = await context.bot.send_message(chat_id='-1001871429218', text=message, parse_mode=constants.ParseMode.HTML)
+
+
+    # await context.bot.delete_message(message_id=last_msg_id, chat_id='-926818356')
+    # msg = await context.bot.send_message(chat_id='-926818356', text=message, parse_mode=constants.ParseMode.HTML)
+    
+    requests.put(
+        f"{domain}/api/setup/1", {'value': msg.message_id})
+
+
+job_queue = app.job_queue
+
+job_minute = job_queue.run_repeating(callback_minute, interval=200, first=1)
+
+app.run_polling()
